d78681a34c01b65c5b103afd0efe424332926b68
joker2770 <1214220480@qq.com>
2023-01-15 18:05:07 +0800

    fix: remove application id. :arrow_down:

--- src/frontend_gtk/main.c
+++ src/frontend_gtk/main.c
@@ -338,7 +338,7 @@ main(int argc, char *argv[])
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new ("com.github.md5", G_APPLICATION_FLAGS_NONE);
+  app = gtk_application_new (NULL, G_APPLICATION_FLAGS_NONE);
   g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
   status = g_application_run (G_APPLICATION (app), argc, argv);
   g_object_unref (app);
